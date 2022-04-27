991b157b1226d7ade47e4bd4fb1259f83c8af17a
kadekdarmayasa <darmayasadiputra@gmail.com>
2022-04-27 15:45:08 +0800

    finishing all sections

--- src/js/flickity.js
+++ src/js/flickity.js
@@ -1,9 +1,17 @@
-function flkty(elemen, wrapAround = true, autoPlay = true) {
+function flkty(elemen, wrapAround = true, autoPlay = true, prevNextButtons = true, pageDots = true) {
 	return new Flickity(elemen, {
 		wrapAround: wrapAround,
 		autoPlay: autoPlay,
+		prevNextButtons: prevNextButtons,
+		pageDots: pageDots,
 	});
 }
 
 const carouselContainer = document.querySelectorAll('.carousel-container');
-carouselContainer.forEach((element) => flkty(element));
+carouselContainer.forEach(function (element) {
+	if (element.classList.contains('carousel-people-testimoni')) {
+		flkty(element, true, false, false, true);
+	} else {
+		flkty(element);
+	}
+});
